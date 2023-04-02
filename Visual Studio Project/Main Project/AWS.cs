715502da08e4afe48be535c5572c32eda800b366
LeoVeroSilva <36208773+LeoVeroSilva@users.noreply.github.com>
2023-04-02 19:44:24 -0300

    update

--- Visual Studio Project/Main Project/AWS.cs
+++ Visual Studio Project/Main Project/AWS.cs
@@ -4,36 +4,40 @@ using System.Threading.Tasks;
 using Amazon;
 using Amazon.Rekognition;
 using Amazon.Rekognition.Model;
-class MyAWSClass
-{
-    static async Task CallService()
+
+namespace MyAWSClassed
+{ 
+    class MyAWSClass
     {
-        var region = RegionEndpoint.USWest2; // Replace with the region where your Rekognition service is located
-        var client = new AmazonRekognitionClient(region); // Create a new Rekognition client
-
-        // Replace with the name of the image file you want to analyze
-        var imagePath = "example.jpg";
-        var imageBytes = File.ReadAllBytes(imagePath);
-
-        var detectProtectiveEquipmentRequest = new DetectProtectiveEquipmentRequest
+        static async Task CallService()
         {
-            Image = new Image { Bytes = new MemoryStream(imageBytes) },
-            SummarizationAttributes = new ProtectiveEquipmentSummarizationAttributes
+            var region = RegionEndpoint.USWest2; // Replace with the region where your Rekognition service is located
+            var client = new AmazonRekognitionClient(region); // Create a new Rekognition client
+
+            // Replace with the name of the image file you want to analyze
+            var imagePath = "example.jpg";
+            var imageBytes = File.ReadAllBytes(imagePath);
+
+            var detectProtectiveEquipmentRequest = new DetectProtectiveEquipmentRequest
             {
-                MinConfidence = 80F // Set the minimum confidence level required for a PPE detection to be returned
-            }
-        };
+                Image = new Image { Bytes = new MemoryStream(imageBytes) },
+                SummarizationAttributes = new ProtectiveEquipmentSummarizationAttributes
+                {
+                    MinConfidence = 80F // Set the minimum confidence level required for a PPE detection to be returned
+                }
+            };
 
-        var detectProtectiveEquipmentResponse = await client.DetectProtectiveEquipmentAsync(detectProtectiveEquipmentRequest);
+            var detectProtectiveEquipmentResponse = await client.DetectProtectiveEquipmentAsync(detectProtectiveEquipmentRequest);
 
-        Console.WriteLine("Detected PPE:");
-        foreach (var bodyPart in detectProtectiveEquipmentResponse.Persons[0].BodyParts)
-        {
-            Console.WriteLine($"  {bodyPart.Name}:");
-
-            foreach (var equipment in bodyPart.EquipmentDetections)
+            Console.WriteLine("Detected PPE:");
+            foreach (var bodyPart in detectProtectiveEquipmentResponse.Persons[0].BodyParts)
             {
-                Console.WriteLine($"    {equipment.Type} (confidence: {equipment.Confidence})");
+                Console.WriteLine($"  {bodyPart.Name}:");
+
+                foreach (var equipment in bodyPart.EquipmentDetections)
+                {
+                    Console.WriteLine($"    {equipment.Type} (confidence: {equipment.Confidence})");
+                }
             }
         }
     }
